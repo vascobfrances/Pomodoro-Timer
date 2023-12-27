380f248be690ec6e3ff928e9424e0f6f3c507b97
vasco <vascobfrances@gmail.com>
2023-12-27 15:02:45 +0000

    contador repetições para controlar o tempo de count down

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def star_timer():
-    count_down(5 * 60)
-
+    global reps
+    work_sec = WORK_MIN * 60
+    short_brack_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 2 == 0:
+        count_down(work_sec)
+    elif reps == 7 :
+        count_down(long_break_sec)
+    elif reps % 2 != 0:
+    count_down(short_brack_sec)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10 :
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(time_text, text=f"{count_min}:{count_sec}")
     if count >0:
         window.after(1000, count_down, count - 1)
